5597a8fe58f47aa036f945daada782aa5ffc8965
jungyeongseo <pp1312pp@naver.com>
2019-08-20 02:07:51 +0900

    upload python

---
+++ advanced_python/star.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+a = 1
+
+
+# In[2]:
+
+
+__b = 2
+
+
+# In[3]:
+
+
+c = '정영서'
+
+
+# In[4]:
+
+
+def x():
+    return 1
+
+
+# In[5]:
+
+
+def _y():
+    return 2
+
+
+# In[ ]:
+
+
+__all__=['a']
+
